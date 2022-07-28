d8e5962541bcd4e86fd196fa47201844ffc40d60
SinedGG <denis2320032@gmail.com>
2022-07-28 19:08:44 +0300

    🍃 | First commit

---
+++ index.js
@@ -0,0 +1,73 @@
+require("dotenv").config();
+const { Telegraf } = require("telegraf"),
+  ytdl = require("ytdl-core"),
+  bot = new Telegraf(process.env.TG_TOKEN),
+  fs = require("fs");
+
+const mysql = require("mysql2/promise");
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+});
+
+const downloader = require("./module/download_audio.js");
+
+bot.on("text", async (ctx) => {
+  try {
+    var url = ctx.message.text;
+    if (!(url.includes("youtube.com") || url.includes("youtu.be")))
+      return ctx.reply("Nope.");
+    const info = await ytdl.getBasicInfo(url);
+    const videoID = info.videoDetails.videoId;
+    console.log(`[${videoID}] Startign task from ${ctx.chat.username} user`);
+    var [rows] = await db.query(
+      `select * from link where video_id = '${videoID}'`
+    );
+    bot.telegram.deleteMessage(ctx.chat.id, ctx.message.message_id);
+    if (rows.length > 0) {
+      bot.telegram.forwardMessage(
+        ctx.chat.id,
+        process.env.data_chat_id,
+        rows[0].message_id
+      );
+    } else {
+      var loading = await bot.telegram.sendMessage(ctx.chat.id, "Loading...");
+      var patch = await downloader(info);
+      var msg = await bot.telegram.sendDocument(process.env.data_chat_id, {
+        source: patch,
+      });
+
+      bot.telegram.forwardMessage(
+        ctx.chat.id,
+        process.env.data_chat_id,
+        msg.message_id
+      );
+      db.query(`insert into link values ('${videoID}', '${msg.message_id}')`);
+      bot.telegram.deleteMessage(ctx.chat.id, loading.message_id);
+      console.log(`[${videoID}] task from complete`);
+      clear(videoID, patch);
+    }
+  } catch (error) {
+    console.log(`Err on task from ${ctx.chat.username}`);
+    var nope = await bot.telegram.sendMessage(ctx.chat.id, "Nope.");
+    setTimeout(() => {
+      bot.telegram.deleteMessage(ctx.chat.id, ctx.message.message_id);
+      bot.telegram.deleteMessage(ctx.chat.id, nope.message_id);
+    }, 15 * 1000);
+  }
+});
+
+function clear(videoID, patch) {
+  try {
+    fs.unlinkSync(`./temp/${videoID}.jpg`);
+    fs.unlinkSync(`./temp/${videoID}.webp`);
+    fs.unlinkSync(patch);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+bot.launch();
---
+++ module/download_audio.js
@@ -0,0 +1,74 @@
+const ytdl = require("ytdl-core"),
+  ffmpeg = require("fluent-ffmpeg"),
+  fs = require("fs"),
+  https = require("https"),
+  sharp = require("sharp"),
+  id3 = require("node-id3").Promise;
+
+function main(info) {
+  return new Promise(async (resolve, reject) => {
+    const art =
+      info.videoDetails.thumbnails[info.videoDetails.thumbnails.length - 1].url;
+    const title = info.videoDetails.title;
+    const author = info.videoDetails.author.name.replace(" - Topic", "");
+    const videoID = info.videoDetails.videoId;
+    const path = `./temp/${author} - ${title}.mp3`;
+
+    var stream = ytdl(info.videoDetails.video_url, {
+      filter: "audioonly",
+    });
+    ffmpeg(stream)
+      .audioBitrate(128)
+      .save(path)
+      .on("error", (err) => {
+        console.log(err);
+      })
+      .on("end", () => {
+        console.log(`[${videoID}] Audio download complete`);
+        download_art();
+      });
+
+    function download_art() {
+      https
+        .get(art, (res) => {
+          const file = fs.createWriteStream(`./temp/${videoID}.webp`);
+          res.pipe(file);
+          file.on("finish", () => {
+            file.close();
+            console.log(`[${videoID}] Art download complete`);
+            crop_art();
+          });
+        })
+        .on("error", (err) => {
+          console.log(err);
+        });
+    }
+
+    function crop_art() {
+      sharp.cache(false);
+      sharp(`./temp/${videoID}.webp`)
+        .resize(1000, 1000)
+        .toFile(`./temp/${videoID}.jpg`)
+        .then(() => {
+          console.log(`[${videoID}] Art cropped for`);
+          set_metadata();
+        });
+    }
+
+    async function set_metadata() {
+      const tags = {
+        title: title,
+        artist: author,
+        APIC: `./temp/${videoID}.jpg`,
+      };
+      try {
+        await id3.write(tags, path);
+      } catch (error) {
+        console.log(err);
+      }
+      resolve(path);
+    }
+  });
+}
+
+module.exports = main;
