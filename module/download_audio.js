6466411b98d22116cbc1cdaa4db9896a45d1d136
SinedGG <denis2320032@gmail.com>
2022-11-02 08:59:07 +0200

    Revert "🍂 | TEMP | Add cookie to request, removed ffmpeg"
    This reverts commit 49b353994a5463cd47685638dcfdcf126196c6b1.

--- module/download_audio.js
+++ module/download_audio.js
@@ -1,4 +1,5 @@
 const ytdl = require("ytdl-core"),
+  ffmpeg = require("fluent-ffmpeg"),
   fs = require("fs"),
   https = require("https"),
   sharp = require("sharp"),
@@ -20,19 +21,17 @@ function main(info) {
 
     var stream = ytdl(info.videoDetails.video_url, {
       filter: "audioonly",
-      requestOptions: {
-        headers: {
-          cookie: process.env.COOKIE,
-        },
-      },
     });
-
-    stream.on("end", () => {
-      console.log(`[${videoID}] Audio download complete`);
-      download_art();
-    });
-
-    stream.pipe(fs.createWriteStream(path));
+    ffmpeg(stream)
+      .audioBitrate(128)
+      .save(path)
+      .on("error", (err) => {
+        console.log(err);
+      })
+      .on("end", () => {
+        console.log(`[${videoID}] Audio download complete`);
+        download_art();
+      });
 
     function download_art() {
       https
